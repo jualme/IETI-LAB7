a41908a9536f44cacd3bf96382d43a9649c8a8bd
Juan Alberto Mejia S <jualme9@hotmail.com>
2020-10-05 12:14:12 -0500

    First commit woring get and post

---
+++ add-task/index.js
@@ -0,0 +1,29 @@
+module.exports = async function (context, req) {
+    context.log('Adding a task to the planner');
+    const task = req.body;
+    const {MongoClient} = require("mongodb");
+    const uri =
+        "mongodb+srv://jualme:Juan241293@cluster0.zcj0p.azure.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        const database = client.db("Cluster0");
+        const collection = database.collection("tasks");
+        const result = await collection.insertOne(task);
+
+        console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,);
+
+    } finally {
+        await client.close();
+        const responseMessage = {response: task};
+        context.res = {
+            status: 201,
+            body: responseMessage
+        };
+    }
+
+}
+
